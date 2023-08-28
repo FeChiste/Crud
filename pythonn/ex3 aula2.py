a234903cad5b4c5b43a39b50845ccb5d767dd4a2
FeChiste <143238511+FeChiste@users.noreply.github.com>
2023-08-28 00:25:48 -0300

    Delete pythonn directory

--- pythonn/ex3 aula2.py
+++
@@ -1,7 +0,0 @@
-inicio = 0
-fim = 1000
-divisor = float(input("Digite um número: "))
-
-for numero in range(inicio, fim):
-    if numero % divisor == 0:
-        print(numero)
--- pythonn/ex5 aula2.py
+++
@@ -1,8 +0,0 @@
-numero = '127957'
-soma = 0
-
-for caractere in numero:
-    valor = int(caractere)
-    soma += valor
-
-print("A soma dos caracteres da string é:", soma)
